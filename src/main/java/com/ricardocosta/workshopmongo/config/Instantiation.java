d65c17043c3aaf716db63f0532fa3b9c3d414d5c
ricardocossta <rm87404@fiap.com.br>
2023-03-24 22:17:31 -0300

    Instanciando base de dados

---
+++ src/main/java/com/ricardocosta/workshopmongo/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.ricardocosta.workshopmongo.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ricardocosta.workshopmongo.domain.User;
+import com.ricardocosta.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		repository.saveAll(List.of(maria, alex, bob));
+		
+	}
+
+}
